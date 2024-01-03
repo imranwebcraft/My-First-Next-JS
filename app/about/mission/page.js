3402b6c0fca9bace87333f9681836a526153da11
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-03 15:59:17 +0600

    learn use of Image component font and image optimization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,10 +1,14 @@
 import Button from '@/app/components/Button';
-import React from 'react';
+import Image from 'next/image';
+import imran from '@/public/imran.png';
 
 const Mission = () => {
 	return (
 		<div>
 			<span className="mt-10">This is Mission page</span>
+			<div className="w-[400px]">
+				<Image placeholder="blur" src={imran} alt="Imran Image" />
+			</div>
 			<Button />
 		</div>
 	);
